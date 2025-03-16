118e3fd23ce84cddd8a4f600e08343039286806c
Eredin Bréacc Glas <74897764+cupsinice@users.noreply.github.com>
2025-03-16 08:17:10 +0300

    edit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,4 +3,10 @@
 Changing a variable later in the code is called "Mutating" just like in python.
 
 123123
+
+you can decalre a string literal like inn python except you do it with backticks``
+
+let myString = `Hi I am ${myName}`;
 */
+
+

0d74d6f53a837c18b60a7580db167978dfc7d882
Eredin Bréacc Glas <74897764+cupsinice@users.noreply.github.com>
2025-03-16 09:08:53 +0300

    edit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,39 @@
 /*
-3 types of decleration keywords (let, const, and var).
-Changing a variable later in the code is called "Mutating" just like in python.
+- Types of decleration keywords (let, const, and var).
 
-123123
+- Changing a variable later in the code is called "Mutating" just like in python.
 
-you can decalre a string literal like inn python except you do it with backticks``
+- Decalre a string literal like inn python except you do it with backticks``
 
-let myString = `Hi I am ${myName}`;
+- let myString = `Hi I am ${myName}`;
+
+- if (){    written this way
+    
+}
+    else if (){
+    
+    }
+    else {
+
+}
+
+- To convert types Number(), String(), Boolean(),
+console.log('1' + 1 + '3') would result in a 4 cuz addition turns strings into a number(Type coerstion).
+
+- Type of NaN is Number (NaN = Not a Number).
+
+- Falsy values are: False, 0, NaN, '', undefined, null. Else everything is Truthy Value.
+
+- '10' == 10 would result in a true (=== strict equality operator doesn't do type coertion and == is the loose equality operator does type coertion)
+AVOID LOOSE EQUALITY OPERATOR WHEN COMPARING
+
+- != loose nequal !== strict nequal. Avoid loose.
+
+- Prompt: (input() in python) reveals a prompt field for the user to write an input in it.
+
+- 
+
+
 */
 
 

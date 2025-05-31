f0ed4216d3475232e97eb49efe67818d1a52acc1
ALUMUX <74897764+cupsinice@users.noreply.github.com>
2025-05-31 18:20:32 +0300

    123

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,49 +1,52 @@
 'use strict';
-
 /*
 console.log(document.querySelector('.message').textContent);
 // DOM = DOCUMENT OBJECT MODEL access html elements and css styles and manipulate them.
 // DOM IS NOT NATIVE TO JAVASCRIPT INSTEAD IT COMES FROM WEBAPI THAT SHIPS WITH BROWESERS.
 */
 
+// Pre Vars
+
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 5;
 console.log(secretNumber);
 document.querySelector('.score').textContent = score;
+
+// Check Logic
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
-  if (guess !== secretNumber) {
-    if (score <= 5 && score !== 0) {
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = `${distance(
-        secretNumber,
-        guess
-      )} ${heat(secretNumber, guess)}`;
-    } else if (score === 0 || score < 0) {
-      document.querySelector('.message').textContent = 'You Lost!';
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Enter A Number!';
+  } else {
+    if (guess !== secretNumber) {
+      if (score <= 5 && score !== 0) {
+        score--;
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.message').textContent = `${distance(
+          secretNumber,
+          guess
+        )} ${heat(secretNumber, guess)}`;
+      } else if (score === 0 || score < 0) {
+        document.querySelector('.message').textContent = 'You Lost!';
+      }
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = 'You Got It!';
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('.highscore').textContent = score;
     }
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'You Got It!';
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
   }
 });
 
-// Again Functionality
+// Again Logic
 
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('body').style.backgroundColor = '#222222';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').textContent = '?';
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  score = 5;
-  document.querySelector('.score').textContent = score;
-  console.log(secretNumber);
+  fagain();
 });
+
+// Functions
 
 function distance(trgt, xpec) {
   if (Math.abs(trgt - xpec) > 5) {
@@ -64,4 +67,15 @@ function heat(trgt, xpec) {
   if (trgt - xpec > 0) {
     return 'Go Higher.';
   }
+}
+
+function fagain() {
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 5;
+  document.querySelector('.score').textContent = score;
+  console.log(secretNumber);
 }
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+// Why do we need a for loop for that?
+// Loop through buttons and attach a listner to the queue
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', function () {
+    console.log('Open Clicked!');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+  btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+}
+
+// Keypressed
+
+document.addEventListener('keypress', function (e) {
+  console.log('Keypressed!');
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      modal.classList.add('hidden');
+      overlay.classList.add('hidden');
+    }
+  }
+});

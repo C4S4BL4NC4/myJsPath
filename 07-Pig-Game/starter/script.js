57f5ad88d5eb29fb519d6aee7f28878939b5fc3b
ALUMUX <74897764+cupsinice@users.noreply.github.com>
2025-06-02 16:27:12 +0300

    123123

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,11 +4,29 @@
 
 let pScore = [0, 0];
 let pTotal = [0, 0];
-const maxScore = 100;
+const maxScore = 30;
 let turn = 0;
-const diceHandle = document.querySelector('.dice');
+
+// Element Handlers
+
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEL = document.querySelector('.dice');
+const newGameEL = document.querySelector('.btn--new');
+const rollBtnEL = document.querySelector('.btn--roll');
+const resetBtnEL = document.querySelector('.btn--new');
+const holdBtnEL = document.querySelector('.btn--hold');
 
 // Functions
+
+function init() {
+  // Initalize HTML values
+
+  score0EL.textContent = 0;
+  score1EL.textContent = 0;
+  diceEL.classList.add('hidden');
+  //newGameEL.classList.add('hidden');
+}
 
 function roll() {
   let rolls = Math.trunc(Math.random() * 6) + 1;
@@ -40,24 +58,46 @@ function roll() {
 function switchTurn(player) {
   if (player === 0) {
     // Player 1
+
     player = 1;
   }
 
   if (player === 1) {
     // Player 2
+
     player = 0;
   }
 }
 
 function addC2T(current, pTotalScore) {
   // Add current to total score of a player
+
   return (pTotalScore += current);
+}
+
+function resetGame() {
+  console.log('Game is Reset');
 }
 
 /* Program Flow */
 
-// Dice Section
+init();
 
-// Switch Section
+// Dice Button
+
+rollBtnEL.addEventListener('click', function () {
+  roll();
+});
+
+// Hold Button
+
+holdBtnEL.addEventListener('click', function () {
+  console.log('Hold Clicked');
+  switchTurn(turn);
+});
 
 // Game Reset
+
+resetBtnEL.addEventListener('click', function () {
+  resetGame();
+});

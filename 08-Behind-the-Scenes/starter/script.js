67b18ac683d210fe0b3daec8e927a7d27b76fa9c
ALUMUX <74897764+cupsinice@users.noreply.github.com>
2025-06-07 03:22:18 +0300

    Shallow Copy and Cloning

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 29,
+  family: ['Alice', 'Bob'],
+};
+
+// Shallow Copy
+const marriedJessica = { ...jessica };
+
+marriedJessica.lastName = 'Davis';
+marriedJessica.family.push('Sam');
+
+console.log(jessica);
+console.log(marriedJessica);
+
+// Deep Copy or Deep Clone
+
+const hammond = {
+  firstName: 'Hammond',
+  lastName: 'Avidson',
+  age: 39,
+  family: ['Moe', 'Martina'],
+};
+
+const hammondClone = structuredClone(hammond);
+hammondClone.family.push('Salah');
+
+console.log(hammond);
+console.log(hammondClone);

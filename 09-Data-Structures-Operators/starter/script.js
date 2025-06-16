c6dadb025a6555ee059ca6410f088a5ab4a8a131
0xN3UR0M4NC3R <sabahsadeq199@gmail.com>
2025-06-16 03:58:00 +0300

    dde

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -784,7 +784,7 @@ console.log(events);
 // 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
 gameEvents.delete(64);
 console.log(gameEvents);
-// 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes).
 console.log(
   `An event happened, on average, every ${90 / gameEvents.size} minutes`
 );

20bde493f2884b7be8ce2fc4ac72df0d93f36260
ALUMUX <74897764+cupsinice@users.noreply.github.com>
2025-06-26 13:31:07 +0300

    123

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+const greet = greeting => {
+  return name => console.log(`${greeting} ${name}`);
+};
+
+const greeterHey = greet('Hey');
+
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Roa');
+greet('Ayo')('Jessie');

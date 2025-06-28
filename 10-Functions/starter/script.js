02bf41c7032183301b934eac52c41c772961257e
ALUMUX <74897764+cupsinice@users.noreply.github.com>
2025-06-28 11:00:12 +0300

    123123

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -128,69 +128,30 @@
 
 // GOOD LUCK 😀
 
-// const poll = {
-//   question: 'What is you favourite programming language?',
-//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-//   answers: new Array(4).fill(0),
-//   registerNewAnswer: function () {
-//     const answer = Number(
-//       prompt(this.question + '\n' + this.options.join('\n'))
-//     );
-
-//     if (typeof answer === 'number') {
-//       if (answer <= 3 && answer >= 0) {
-//         this.answers[answer]++;
-//       }
-//     }
-//     this.displayResults;
-//   },
-//   displayResults: function (answers, type) {
-//     if (typeof type === 'string') {
-//       if (type === 'string') {
-//         console.log(answers.join(', '));
-//       } else if (type === 'array') {
-//         console.log(this.answers);
-//       }
-//     }
-//   },
-// };
-// document
-//   .querySelector('.poll')
-//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
-
-// poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
-// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
-// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
-
 const poll = {
-  question: 'What is your favourite programming language?',
+  question: 'What is you favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    // Get answer
+  registerNewAnswer: function () {
     const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
-      )
+      prompt(this.question + '\n' + this.options.join('\n'))
     );
-    console.log(answer);
 
-    // Register answer
-    typeof answer === 'number' &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
-
+    if (typeof answer === 'number') {
+      if (answer <= this.answers.length && answer >= 0) {
+        this.answers[answer]++;
+      }
+    }
     this.displayResults();
     this.displayResults('string');
   },
-
-  displayResults(type = 'array') {
-    if (type === 'array') {
-      console.log(this.answers);
-    } else if (type === 'string') {
-      // Poll results are 13, 2, 4, 1
-      console.log(`Poll results are ${this.answers.join(', ')}`);
+  displayResults: function (type = 'array') {
+    if (typeof type === 'string') {
+      if (type === 'string') {
+        console.log(this.answers.join(', '));
+      } else if (type === 'array') {
+        console.log(this.answers);
+      }
     }
   },
 };

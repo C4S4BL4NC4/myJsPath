ce10ebc5bfa39976cf3a6f2b4e33a2afb3055db4
ALUMUX <74897764+cupsinice@users.noreply.github.com>
2025-08-08 13:34:57 +0300

    Internationalization

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -22,11 +22,11 @@ const account1 = {
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2025-08-04T23:36:17.929Z',
+    '2025-08-07T10:51:36.790Z',
   ],
   currency: 'EUR',
-  locale: 'pt-PT', // de-DE
+  locale: 'en-GB', // de-DE
 };
 
 const account2 = {
@@ -81,6 +81,24 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = `${date.getFullYear()}`;
+
+    return `${day}/${month}/${year}`;
+  }
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -96,13 +114,14 @@ const displayMovements = function (acc, sort = false) {
     const type = movement > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(movementDate);
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__date">${date.getDate()}/${date.getMonth()}/${date.getFullYear()}</div>
+        <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${movement.toFixed(2)}€</div>
       </div>
     `;
@@ -164,6 +183,8 @@ const updateUI = function (acc) {
 // Event handlers
 let currentAccount;
 
+// Expermenting API Internat
+
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
@@ -171,6 +192,23 @@ btnLogin.addEventListener('click', function (e) {
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value
   );
+
+  // As of CurrentDate
+
+  const now = new Date();
+  const options = {
+    hour: 'numeric',
+    minute: 'numeric',
+    day: 'numeric',
+    month: 'short', // 2-digit, long
+    year: 'numeric',
+    weekday: 'short',
+  };
+
+  labelDate.innerHTML = `${new Intl.DateTimeFormat(
+    currentAccount.locale,
+    options
+  ).format(now)}`;
   console.log(currentAccount);
 
   if (currentAccount?.pin === +inputLoginPin.value) {
@@ -261,14 +299,6 @@ btnSort.addEventListener('click', function (e) {
 
   sorted = !sorted;
 });
-
-// As of CurrentDate
-
-labelDate.innerHTML = `${new Date(Date.now()).getFullYear()}/${new Date(
-  Date.now()
-).getUTCMonth()}/${new Date(Date.now()).getDate()}, ${new Date(
-  Date.now()
-).getHours()}:${new Date(Date.now()).getMinutes()}`;
 
 // Movements Date
 
@@ -403,3 +433,18 @@ labelDate.innerHTML = `${new Date(Date.now()).getFullYear()}/${new Date(
 // console.log(future); // Tue Dec 04 2040 12:43:58 GMT+0300 (GMT+03:00)
 
 // console.log(new Date('2019-11-18T21:31:17.178Z').getFullYear());
+
+//////////////////////////////////////
+// Operations with Dates
+
+// const future = new Date(2024, 2, 1, 4, 0, 0);
+// console.log(+future); // Converting into a number turns it into a timestamp instead of a stringated date 2332835038000
+// console.log(Number(future)); // Converting into a number turns it into a timestamp instead of a stringated date 2332835038000
+
+// const daysPassed = (fromDate, toDate = Date.now()) =>
+//   Math.abs(fromDate - toDate) / (1000 * 60 * 60 * 24);
+
+// console.log(daysPassed(new Date(2023, 2, 6), new Date(2034, 6, 18)));
+
+//////////////////////////////////////
+// Internationalization: (formating dates and numbers)

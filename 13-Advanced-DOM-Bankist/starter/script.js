ba0f2471d6e1b83df01e86636bbcf68a17ff9b6a
ALUMUX <74897764+cupsinice@users.noreply.github.com>
2025-08-11 15:52:25 +0300

    PageOFFSETSXY

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -90,3 +90,27 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log(
+    `Scroll X/Y offsets: ${window.pageXOffset} / ${window.pageYOffset}`
+  );
+  console.log(
+    `height/width of viewport: ${document.documentElement.clientHeight} ${document.documentElement.clientWidth}`
+  );
+
+  // Scrolling
+
+  window.scrollTo(
+    s1coords.left + window.pageXOffset,
+    s1coords.top + window.pageYOffset
+  );
+});

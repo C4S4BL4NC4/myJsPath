b1a484a1efb4d34323633494c81e8b6357da9d1e
C4S4BL4NC4 <sabahsadeq199@gmail.com>
2025-08-30 17:17:43 +0300

    leaflet app

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -16,8 +16,21 @@ navigator.geolocation.getCurrentPosition(
     const { latitude } = pos.coords;
     const { longitude } = pos.coords;
     console.log(latitude, longitude);
+    const cords = [latitude, longitude];
+
+    const map = L.map('map').setView(cords, 13); // ID in html called map
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(cords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
   },
   function () {
-    alert("can't get postion");
+    alert("can't get position");
   }
 );

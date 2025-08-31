0e15976d057ab629ec97a72d505f1c7c5ee79b00
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2025-08-31 15:01:04 +0300

    popups

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,13 +11,12 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(
+const cords = navigator.geolocation.getCurrentPosition(
   function (pos) {
     const { latitude } = pos.coords;
     const { longitude } = pos.coords;
     console.log(latitude, longitude);
     const cords = [latitude, longitude];
-
     const map = L.map('map').setView(cords, 13); // ID in html called map
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -25,10 +24,24 @@ navigator.geolocation.getCurrentPosition(
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(cords)
-      .addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup();
+    map.on('click', function (mapEvent) {
+      console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    });
   },
   function () {
     alert("can't get position");

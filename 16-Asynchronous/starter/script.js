0e2b3c66e6ab5e6f5fcda2691eda0b092aa5f56f
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2025-09-14 13:27:34 +0300

    fetch and promise handling

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -121,3 +121,45 @@ getCountryAndNeighbor('Argentina');
 // Callback Hell = (Recursive Callbacks) (Nested Callbacks)
 
 // Promisies are used to avoid callback hell.
+
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)} M</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+              </div>
+            </article>
+        `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountry = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data[0]);
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      renderCountry(data, 'neighbour');
+    });
+};
+
+getCountry('china');

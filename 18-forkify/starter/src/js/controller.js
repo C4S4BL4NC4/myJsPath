20ef4e86b08b6dc66e9bd0c045413a2568fafaf1
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2025-09-20 12:12:03 +0300

    vicky

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -12,3 +12,18 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886zzzz`
+    );
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(`💥${data.message} (${res.status})`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+showRecipe();
